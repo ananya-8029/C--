3eb28e3d6e1e9c93273617e1040c4ef803d4cbb2
Ananya Gupta <anaya29003@gmail.com>
2023-08-12 15:24:02 +0530

    12-August

--- Tree/Binary Tree To Linked List.cpp
+++ Tree/Binary Tree To Linked List.cpp
@@ -47,3 +47,35 @@ public:
         }
     }
 };
+
+//GFG SOLUTION
+//Idea is to do a recursive call for left subtree and make all the left node right and all the left point to NULL.
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        //check for null or a leaf node
+        if(root==NULL || root->left==NULL && root->right==NULL) return;
+
+        //now we have to make left node null.So make all the left node to right
+        if(root->left!=NULL){
+            //see for the last node in the left
+            flatten(root->left);
+
+            //store the right of the node in a variale and move left valuse to right and make left as null
+            TreeNode* temp=root->right;
+            root->right=root->left;
+            root->left=NULL;
+
+            //now we have to move the right of the node to the last of the right node.We simply need to traverse the right from the root
+            TreeNode* node=root->right;
+            while(node->right!=NULL){
+                node=node->right;
+            }
+
+            //we have the position where the node have to be inserted
+            node->right=temp;
+        }
+        //now traverse for the right
+        flatten(root->right);
+    }
+};
